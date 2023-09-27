54b96c51816b5e0f94dc1b7c4529535915252ac3
huss <huss.jber@hotmail.com>
2023-09-27 16:31:53 +0300

    D1

---
+++ day-1/1.1.js
@@ -0,0 +1,18 @@
+// 1 String
+let say= "HelloWorld";
+console.log(typeof say);
+
+// 2 Number
+let age= 42;
+console.log(typeof age);
+
+// 3 Object
+let object= { key: "value" };
+
+// 4 Boolean
+let sick= true;
+console.log(typeof sick);
+
+// 5 Symbol
+let dollar= Symbol("$");
+console.log(typeof dollar);
